77f7160575004dc039362949f76b990873a3df95
michalswig <mike@data.pl>
2026-05-20 22:05:37 +0200

    git init

---
+++ src/main/java/com/mike/shopapp/ShopAppApplication.java
@@ -0,0 +1,13 @@
+package com.mike.shopapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopAppApplication.class, args);
+    }
+
+}
